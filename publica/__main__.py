678f6a9ebcbefc7b9fb68e2b2275562fa042d25d
woshishabii <3488229708@qq.com>
2023-12-03 18:46:19 +0800

    initial commit

---
+++ publica/__main__.py
@@ -0,0 +1,1 @@
+# Okay a new beginning
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='publica',
+    version='0.0.0',
+    author='Rain Star',
+    description="Yet another library server for home",
+    packages=find_packages(),
+    install_requires=[],
+)
